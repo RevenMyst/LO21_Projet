451e0496cf437e58d375f1e37594a17c83bd4c91
VonMint <vonmint@hotmail.com>
2020-11-21 16:57:30 +0100

    Update RationalLit.h
    Ajout de Constructeur et redefinition du methode toString

--- RationalLit.h
+++ RationalLit.h
@@ -7,6 +7,17 @@ class LitRational : public Litteral
 	unsigned int denominateur;
 	LitType Type = RATIONALLIT;
 public:
+	LitRational(int n, unsigned int d) : numerateur(n), denominateur(d) {}
 	int getNum() { return numerateur; }
 	unsigned int getDen() { return denominateur; }
+	std::string toString(int size) {
+		//Cas d'ou on a un entier
+		if (getDen() == 1)
+			return(std::to_string(getNum()));
+		//Cas d'ou on a meme valeur dans les deux parties ex: 5/5
+		if (getDen() == getNum())
+			return("1");
+		std::string res = std::to_string(getNum()) + "/" + std::to_string(getDen());
+		return(res);
+	}
 };
