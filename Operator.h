a7a32e3cff6421fb1394fc848d1cd2a6c209d8e9
VonMint <vonmint@hotmail.com>
2020-11-21 13:00:13 +0100

    Update Operator.h
    definition des methodes et attributs ainsi que la definition ifndef

--- Operator.h
+++ Operator.h
@@ -1,6 +1,20 @@
+#ifndef OPERATOR_H
+#define OPERATOR_H
+
 #pragma once
 #include "Operand.h"
+
 class Operator : public Operand
 {
+protected:
+	std::string Symbole;
+private:
+	bool verify();
+public:
+	std::string toString();
+	void exec();
+protected:
+	void ope();
 };
 
+#endif
