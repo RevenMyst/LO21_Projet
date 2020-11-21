82688a817d68e7076ec401b92d5ea8d54dd73062
VonMint <vonmint@hotmail.com>
2020-11-21 17:07:47 +0100

    Update LitReel.cpp

--- LitReel.cpp
+++ LitReel.cpp
@@ -1,10 +1,17 @@
 #include "LitReel.h"
 
 double LitReel::getValue() {
+	//Taille maximale si on a un int de 10 chiffres et une mantisse de 10 chiffres
+	int i = 20;
+	std::string stringValue = LitReel::toString(i);
+	double res = stod(stringValue);
+	return(res);
+}
+
+std::string LitReel::toString(int size) {
 	std::string valeur = std::to_string(integer).append(".");
 	std::string virgule = std::to_string(mantisse);
 	if (virgule != "0")
 		valeur = valeur.append(virgule);
-	double res = stod(valeur);
-	return(res);
+	return(valeur);
 }
