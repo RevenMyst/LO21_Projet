f01b8724286d1cfb38e8a3289d9404c8bb86f2fc
VonMint <vonmint@hotmail.com>
2020-11-21 16:44:04 +0100

    Update LitInt.h
    Ajout Constructeur, Redefinition de methode toString et Ajout de Type

--- LitInt.h
+++ LitInt.h
@@ -5,6 +5,12 @@
 class LitInt : public Litteral
 {
 	int value;
+	LitType Type = INTLIT;
 public:
+	LitInt(int v) : value(v){}
 	int getValue() { return value; }
+	std::string toString(int size) {
+		std::string res = std::to_string(value);
+		return(res);
+	}
 };
