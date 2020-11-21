72d23189b676b25796b6f433465c525487a23e6f
VonMint <vonmint@hotmail.com>
2020-11-21 13:00:08 +0100

    Update Litteral.h
    definition des methodes et attributs ainsi que la definition ifndef

--- Litteral.h
+++ Litteral.h
@@ -1,6 +1,19 @@
+#ifndef LITTERAL_H
+#define LITTERAL_H
+
 #pragma once
 #include "Operand.h"
+const enum LitType { INTLIT, REALLIT, RATIONALLIT, EXPLIT, PROGLIT };
+
 class Litteral : public Operand
 {
+protected:
+	unsigned int arite = 0;
+	LitType Type;
+public:
+	void accept(Visitor visitor);
+	LitType getClass() { return Type; }
+	void exec();
 };
 
+#endif
