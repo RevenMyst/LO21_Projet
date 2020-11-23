a78d5f45613a729eec07b6c4d428a3aee0eedef2
VonMint <vonmint@hotmail.com>
2020-11-23 18:06:25 +0100

    Update RealLit.cpp
    -Changement de Reference RealLit
    -Changement de getValue par getValueR
    -Ajoutant de definition methode getValue pour l'evaluation des operateurs logiques

--- RealLit.cpp
+++ RealLit.cpp
@@ -1,6 +1,6 @@
-#include "LitReel.h"
+#include "RealLit.h"
 
-double LitReel::getValue() {
+double LitReel::getValueR() {
 	//Taille maximale si on a un int de 10 chiffres et une mantisse de 10 chiffres
 	int i = 20;
 	std::string stringValue = LitReel::toString(i);
@@ -14,4 +14,16 @@ std::string LitReel::toString(int size) {
 	if (virgule != "0")
 		valeur = valeur.append(virgule);
 	return(valeur);
+}
+
+int LitReel::getValue() {
+	//Conversion a string des parties du reel
+	std::string s1 = std::to_string(getInt());
+	std::string s2 = std::to_string(getMant());
+
+	//Concatenation des chaines
+	std::string s = s1 + s2;
+
+	//Conversion de retour a int pour l'evaluation
+	return(stoi(s));
 }
