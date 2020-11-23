a0e377e20ce5ce73d4185d242cc306da0bb22573
VonMint <vonmint@hotmail.com>
2020-11-23 18:07:37 +0100

    Update RealLit.h
    -Changement d'heritage a LitteralNumerique
    -Declaration de getInt et getMant utilisé par la methode getValue
    -Declaration de getValue (operateurs logiques) et getValueR (retourne la valeur en double)

--- RealLit.h
+++ RealLit.h
@@ -1,13 +1,16 @@
 #pragma once
 #include "Litteral.h"
 
-class LitReel : public Litteral
+class LitReel : public LitteralNumerique
 {
 	int integer;
 	unsigned int mantisse;
 	LitType Type = REALLIT;
 public:
 	LitReel(int i, unsigned int m) : integer(i), mantisse(m) {}
-	double getValue();
+	int getInt() const { return integer; }
+	unsigned int getMant() const { return mantisse; }
+	double getValueR();
+	int getValue();
 	std::string toString(int size);
 };
