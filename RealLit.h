e42c5a3770c360852ee701a6156ee018ee91c594
VonMint <vonmint@hotmail.com>
2020-11-23 18:14:25 +0100

    Update RealLit.h

--- RealLit.h
+++ RealLit.h
@@ -10,7 +10,7 @@ public:
 	LitReel(int i, unsigned int m) : integer(i), mantisse(m) {}
 	int getInt() const { return integer; }
 	unsigned int getMant() const { return mantisse; }
-	double getValueR();
-	int getValue();
-	std::string toString(int size);
+	double getValueR() const;
+	int getValue() const;
+	std::string toString(int size) const;
 };
