3727f3bb7bd0e3e94ad39b66ff686a41abed8203
VonMint <vonmint@hotmail.com>
2020-12-01 23:53:48 +0100

    Update LitFactory.cpp

--- LitFactory.cpp
+++ LitFactory.cpp
@@ -34,3 +34,26 @@ std::map<std::string, LitFactory*> LitFactory::getLitFactories(){
 bool LitFactory::isLit(std::string Lit){
   return getLitFactories().count(Lit) > 0;
 }
+
+Litteral* RATFactory::getLitteral(std::string rat){
+  using namespace std;
+  int num, den;
+  int nombre = 0;
+  bool neg = false;
+  for (int i = 0; i < (int)rat.length(); i++){
+    if(rat[i] == '-') neg = true;
+    if(isdigit(rat[i]))
+    //Il faut eliminer le caractere '0': '1' - '0' = 1
+      nombre = (nombre * 10) + (rat[i] - '0');
+    else{
+      if(nombre > 0) nombre = num; //On garde le premier nombre comme notre numerateur
+      if(neg == true){
+          num *= -1;
+          neg = false;
+      }
+    }
+  }
+  nombre = den;
+  if(neg == true) den *= -1;
+  return new RationalLit(num, den);
+}
