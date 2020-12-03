19d03c296f0828aeb0e741c105a3638e76a8a061
VonMint <vonmint@hotmail.com>
2020-12-03 19:30:36 +0100

    Update LitFactory.h

--- LitFactory.h
+++ LitFactory.h
@@ -7,37 +7,42 @@ class LitFactory {
 protected:
 	LitFactory() = default;
 	virtual ~LitFactory() {}
-	virtual Litteral* getLitteral() = 0;
+	virtual Operand* getLitteral() = 0;
 	static std::map<std::string, LitFactory*> getLitFactories();
-	static bool isLit(std::string Lit);
 };
 
 class INTFactory : public LitFactory {
 public:
 	INTFactory() = default;
-	Litteral* getLitteral() override { return new LitInt(); }
+	Operand* getLitteral() override { return new LitInt(); }
 };
 
 class REALFactory : public LitFactory {
 public:
 	REALFactory() = default;
-	Litteral* getLitteral() override { return new RealLit(); }
+	Operand* getLitteral() override { return new RealLit(); }
 };
 
 class RATFactory : public LitFactory {
 public:
 	RATFactory() = default;
-	Litteral* getLitteral(std::string rat);
+	Operand* getLitteral(std::string rat);
 };
 
 class EXPFactory : public LitFactory {
 public:
 	EXPFactory() = default;
-	Litteral* getLitteral() override { return new ExpLit(); }
+	Operand* getLitteral() override { return new ExpLit(); }
 };
 
 class PROGFactory : public LitFactory {
 public:
 	PROGFactory() = default;
-	Litteral* getLitteral() override { return new RealLit(); }
+	Operand* getLitteral() override { return new ProgLit(); }
+};
+
+class ATOMFactory : public LitFactory {
+public:
+	PROGFactory() = default;
+	Operand* getLitteral() override { return new Operand(); }
 };
